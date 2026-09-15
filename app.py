67bdc4b8aa210969b7c63f01b243f9c7d4eef1b0
abdoo72007-ux <abdoo72007@gmail.com>
2026-09-15 18:25:15 +0300

    fix number input step

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ st.title("Mobile Price Range Prediction")
 columns = st.columns(len(features))
 
 # تخصيص خانة لكل ميزة (Feature) بالقيم الطبيعية المناسبة لها
-battery_power = columns[0].number_input("battery_power", min_value=500.0, max_value=6000.0, value=1500.0)
-ram = columns[1].number_input("ram", min_value=250.0, max_value=4000.0, value=1000.0)
-px_height = columns[2].number_input("px_height", min_value=0.0, max_value=2000.0, value=500.0)
-px_width = columns[3].number_input("px_width", min_value=0.0, max_value=2000.0, value=1000.0)
-mobile_wt = columns[4].number_input("mobile_wt", min_value=80.0, max_value=250.0, value=150.0)
+battery_power = columns[0].number_input("battery_power", min_value=500.0, max_value=6000.0, value=1500.0, step=10.0)
+ram = columns[1].number_input("ram", min_value=250.0, max_value=4000.0, value=1000.0, step=10.0)
+px_height = columns[2].number_input("px_height", min_value=0.0, max_value=2000.0, value=500.0, step=1.0)
+px_width = columns[3].number_input("px_width", min_value=0.0, max_value=2000.0, value=1000.0, step=1.0)
+mobile_wt = columns[4].number_input("mobile_wt", min_value=80.0, max_value=250.0, value=150.0, step=1.0)
 
 # تجميع القيم في DataFrame بنفس ترتيب الـ features
 df = pd.DataFrame([[battery_power, ram, px_height, px_width, mobile_wt]], columns=features)
